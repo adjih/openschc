8d4f98a60848dd0719f825b53545bf96caed0c2a
Daniel Ducuara <38841094+DanielDucuara2018@users.noreply.github.com>
2019-07-20 01:08:31 +0200

    tried to do better integration (First version Rule manager)

--- src/test_frag.py
+++ src/test_frag.py
@@ -11,32 +11,32 @@ from rulemanager import RuleManager
 from stats.statsct import Statsct
 
 #---------------------------------------------------------------------------
-# Rules
+# RULES
 rule_context = {
     "devL2Addr": "*",
     "dstIID": "*"
 }
 
 compress_rule = {
-    "ruleLength": 3,
-    "ruleID": 5,
-    "compression": {
+    "RuleIDLength": 3,
+    "RuleID": 5,
+    "Compression": {
         "rule_set": []
     }
 }
 
 frag_rule1 = {
-    "ruleLength": 6,
-    "ruleID": 1,
+    "RuleIDLength": 6,
+    "RuleID": 1,
     "profile": { "L2WordSize": 8 },
-    "fragmentation": {
+    "Fragmentation": {
         "FRMode": "ackOnError",
         "FRModeProfile": {
             "dtagSize": 2,
-            "WSize":3, # Number of tiles per window
+            "WSize": 7, # Number of tiles per window
             "FCNSize": 3, # 2^3-2 .. 0 number of sequence de each tile
             "ackBehavior": "afterAll1",
-            "tileSize": 392, # size of each tile -> 9 bits or 392 bits
+            "tileSize": 9, # size of each tile -> 9 bits or 392 bits
             "MICAlgorithm": "crc32",
             "MICWordSize": 8
         }
@@ -44,17 +44,17 @@ frag_rule1 = {
 }
 
 frag_rule2 = {
-    "ruleLength": 6,
-    "ruleID": 2,
+    "RuleIDLength": 6,
+    "RuleID": 2,
     "profile": { "L2WordSize": 8 },
-    "fragmentation": {
+    "Fragmentation": {
         "FRMode": "ackOnError",
         "FRModeProfile": {
             "dtagSize": 2,
-            "WSize": 3, # Number of tiles per window
+            "WSize": 7, # Number of tiles per window
             "FCNSize": 3, # 2^3-2 .. 0 number of sequence de each tile
             "ackBehavior": "afterAll1",
-            "tileSize": 392, # size of each tile -> e.g. 9 bits or 392 bits
+            "tileSize": 9, # size of each tile -> e.g. 9 bits or 392 bits
             "MICAlgorithm": "crc32",
             "MICWordSize": 8
         }
@@ -62,12 +62,12 @@ frag_rule2 = {
 }
 
 frag_rule3 = {
-    "ruleLength": 6,
-    "ruleID": 1,
+    "RuleIDLength": 6,
+    "RuleID": 1,
     "profile": {
         "L2WordSize": 8
     },
-    "fragmentation": {
+    "Fragmentation": {
         "FRMode": "noAck",
         "FRModeProfile": {
             "dtagSize": 2,
@@ -79,12 +79,12 @@ frag_rule3 = {
 }
 
 frag_rule4 = {
-    "ruleLength": 6,
-    "ruleID": 2,
+    "RuleIDLength": 6,
+    "RuleID": 2,
     "profile": {
         "L2WordSize": 8
     },
-    "fragmentation": {
+    "Fragmentation": {
         "FRMode": "noAck",
         "FRModeProfile": {
             "dtagSize": 2,
@@ -95,43 +95,6 @@ frag_rule4 = {
     }
 }
 
-#--------------------------------------------------
-# Main configuration
-
-ack_on_error = True
-packet_loss_simulation = True
-payload_file_simulation = False # Configure the rules, l2_mtu and SF to support the use of a 1400 bytes file
-
-#--------------------------------------------------
-# General configuration
-
-l2_mtu = 408 # bits
-data_size = 255 # bytes
-SF = 12
-
-simul_config = {
-    "log": True,
-}
-
-#---------------------------------------------------------------------------
-# Configuration packets loss
- 
-if packet_loss_simulation:
-    # Configuration with packet loss in noAck and ack-on-error
-    loss_rate = 15 # in %
-    collision_lambda = 0.1
-    background_frag_size = 54
-    loss_config = {"mode":"rate", "cycle":loss_rate}
-    #loss_config = {"mode":"collision", "G":collision_lambda, "background_frag_size":background_frag_size}
-else:
-    # Configuration without packet loss in noAck and ack-on-error
-    loss_rate = None
-    loss_config = None 
-
-#---------------------------------------------------------------------------
-# Init packet loss
-if loss_config is not None:
-    simul_config["loss"] = loss_config
 #---------------------------------------------------------------------------
 
 def make_node(sim, rule_manager, devaddr=None, extra_config={}):
@@ -145,36 +108,63 @@ def make_node(sim, rule_manager, devaddr=None, extra_config={}):
 #---------------------------------------------------------------------------
 # Statistic module
 Statsct.initialize()
-Statsct.log("Statsct test")
-Statsct.set_packet_size(data_size)
-Statsct.set_SF(SF)
-
 #---------------------------------------------------------------------------
 # Fragmentation mode 
 
+ack_on_error = False
 #no-ack
 rm0 = RuleManager()
 rm0.add_context(rule_context, compress_rule, frag_rule3, frag_rule4)
+##rm0.Add(dev_info=frag_rule3)
+##rm0.Add(dev_info=frag_rule4)
 
 rm1 = RuleManager()
 rm1.add_context(rule_context, compress_rule, frag_rule4, frag_rule3)
+##rm1.Add(dev_info=frag_rule4)
+##rm1.Add(dev_info=frag_rule3)
+
 #ack-on-error
 if ack_on_error:  
 
     rm0 = RuleManager()
     rm0.add_context(rule_context, compress_rule, frag_rule1, frag_rule2)
+    ##rm0.Add(dev_info=frag_rule1)
+    ##m0.Add(dev_info=frag_rule2)
 
     rm1 = RuleManager()
     rm1.add_context(rule_context, compress_rule, frag_rule2, frag_rule1)
+    ##rm1.Add(dev_info=frag_rule2)
+    ##rm1.Add(dev_info=frag_rule1)
+
+
+#--------------------------------------------------
+# General configuration
+
+l2_mtu = 56 # bits
+data_size = 14 # bytes
+
+simul_config = {
+    "log": True,
+}
 
 #---------------------------------------------------------------------------
+# Packets loss
+
+#loss_rate = 2 # in %
+loss_rate = None 
+
+#loss_config = {"mode":"rate", "cycle":loss_rate}
+loss_config = None
+
+if loss_config is not None:
+    simul_config["loss"] = loss_config
+#---------------------------------------------------------------------------
 # Configuration of the simulation
-Statsct.get_results() 
+
 sim = simul.Simul(simul_config)
 
-devaddr = b"\xaa\xbb\xcc\xdd"
-node0 = make_node(sim, rm0, devaddr)                   # SCHC device
-node1 = make_node(sim, rm1, devaddr) # SCHC gw
+node0 = make_node(sim, rm0)                   # SCHC device
+node1 = make_node(sim, rm1, devaddr=node0.id) # SCHC gw
 sim.add_sym_link(node0, node1)
 node0.layer2.set_mtu(l2_mtu)
 node1.layer2.set_mtu(l2_mtu)
@@ -192,33 +182,13 @@ print("-------------------------------- Rules -----------------------------")
 print("rules -> {}, {}".format(rm0.__dict__, rm1.__dict__))
 print("")
 
-#device rule
-print("-------------------------------- Device Rule -----------------------------")  
-for rule1 in rm0.__dict__:
-    print(rm0.__dict__[rule1])
-    for info in rm0.__dict__[rule1]:
-        print("info -> {}".format(info))
-        Statsct.set_device_rule(info)
-
-#gw rule
-print("-------------------------------- gw Rule -----------------------------")  
-for rule1 in rm1.__dict__:
-    print(rm1.__dict__[rule1])
-    for info in rm1.__dict__[rule1]:
-        print("info -> {}".format(info))
-        Statsct.set_gw_rule(info)
-#---------------------------------------------------------------------------
-# Statistic configuration
-       
-Statsct.setSourceAddress(node0.id)
-Statsct.setDestinationAddress(node1.id)
-
 #--------------------------------------------------
 # Payload (information in a file or bytearray of "datasize" bytes)
 
+test_file = False
 fileToSend = "testfile_large.txt"
 
-if payload_file_simulation:
+if test_file:
     file = open(fileToSend, 'r') # 1400 bytes   
     payload = file.read().encode()    
     print("Payload size:", len(payload),"bytes")
@@ -229,37 +199,20 @@ else:
     print("Payload size:", len(payload)) 
     print("Payload: {}".format(b2hex(payload)))
     print("")
+
+payload = bytearray(b"""`\
+\x12\x34\x56\x00\x1e\x11\x1e\xfe\x80\x00\
+\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x01\xfe\x80\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x00\x00\x00\x02\x16\
+2\x163\x00\x1e\x00\x00A\x02\x00\x01\n\xb3\
+foo\x03bar\x06ABCD==Fk=eth0\xff\x84\x01\
+\x82  &Ehello""")
 #---------------------------------------------------------------------------
 # Simnulation
 
 node0.protocol.layer3.send_later(1, node1.layer3.L3addr, payload)
 
-Statsct.addInfo('real_packet', payload)
-Statsct.addInfo('real_packet_size', len(payload))
-
 sim.run()
 
-print('-------------------------------- Simulation ended -----------------------|')
 #---------------------------------------------------------------------------
-# Results
-print("")
-print("")
-print("-------------------------------- Statistics -----------------------------")         
-
-print('---- Sender Packet list ')
-Statsct.print_packet_list(Statsct.sender_packets)
-print('')
-
-print('---- Receiver Packet list ')
-Statsct.print_packet_list(Statsct.receiver_packets)
-print('')
-
-print('---- Packet lost Results (Status -> True = Received, False = Failed) ')
-Statsct.print_ordered_packets()
-print('')
-
-print('---- Performance metrics')
-params = Statsct.calculate_tx_parameters()
-print('')
-
-print("---- General result of the simulation {}".format(params))
